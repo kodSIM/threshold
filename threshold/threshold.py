60d0850fc654dd499d4b58cbaa7f1c1ea3057f33
kodSIM <stalker_sim@mail.ru>
2019-08-09 22:47:43 +0300

    Calculate critical values with percentile

--- threshold/threshold.py
+++ threshold/threshold.py
@@ -7,25 +7,27 @@ import pandas as pd
 
 @click.command()
 @click.argument('path', type=click.Path(exists=True))
-@click.option('--cpu_limit', default=45, help='CPU usage limit for test')
-@click.option('--memory_limit', default=10453370, help='Memory usage limit for test')
+@click.option('--cpu_limit', default=0.9, help='Percentile for CPU usage limit for test')
+@click.option('--memory_limit', default=0.9, help='Percentile for memory usage limit for test')
 def threshold(path, cpu_limit, memory_limit):
     """Find critical CPU and memory usage for test.
 
     :param str path: Path to log file.
-    :param int cpu_limit: CPU usage limit for test.
-    :param int memory_limit: Memory usage limit for test.
+    :param int cpu_limit: Percentile for CPU usage limit for test.
+    :param int memory_limit: Percentile for memory usage limit for test.
 
     :return: Exit code. '0' if test have not crossed limits, '1' otherwise.
     :rtype: int
     """
     df = pd.read_csv(path)
-    crit_cpu = df['timestamp'][df['cpu_process %'] > cpu_limit]
-    crit_mem = df['timestamp'][df['memory_process'] > memory_limit]
-    if crit_cpu.size or crit_mem.size:
+    crit_cpu = df['cpu_process %'].quantile([0, cpu_limit])[cpu_limit]
+    crit_mem = df['memory_process'].quantile([0, memory_limit])[memory_limit]
+    anomal_cpu = df[df['cpu_process %'] > crit_cpu]
+    anomal_mem = df[df['memory_process'] > crit_mem]
+    if anomal_cpu.size or anomal_mem.size:
         print('Test is crossed of limits')
-        print(pd.merge(df[df['timestamp'].isin(crit_cpu)],
-                       df[df['timestamp'].isin(crit_mem)],
+        print(pd.merge(anomal_cpu,
+                       anomal_mem,
                        how='outer'))
         sys.exit(1)
     print('Test is success done')
